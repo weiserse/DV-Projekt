5a68a4a881de56c37f02fac658bbf815a40c73b1
SelinaPeukert <101878455+SelinaPeukert@users.noreply.github.com>
2022-04-10 13:40:06 +0200

    Gewinnerfenster hinzugefügt
    Gewinnerfenster in gleichen Ordner wie Welcomescreen.
    Anpassungen, sodass Methoden und Aufrufe zwischen den Klassen funktionieren werden noch angepasst

---
+++ 2_Quellcode/ApplicationTemplate/src/Draw.java
@@ -0,0 +1,29 @@
+package pack1;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JLabel;
+
+public class Draw extends JLabel{
+	
+	ImageLoader il = new ImageLoader();
+	ImageLoader il2 = new ImageLoader();
+	
+	protected void paintComponent (Graphics g) {
+		super.paintComponent (g);
+		
+		Graphics2D g2d = (Graphics2D) g;
+		
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		
+		g.drawImage(il.image, 112,200, 350, 200, null);
+		//g.drawImage(il2.image, 0, 0, 600, 600, null);
+		
+		repaint();
+	
+}
+	
+	
+}
