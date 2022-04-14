2869917652df9f90236f68452bee26ea144b08f3
SelinaPeukert <101878455+SelinaPeukert@users.noreply.github.com>
2022-04-14 14:56:49 +0200

    pack1 entfernen
    war fehlerhaft übernommen

--- 2_Quellcode/ApplicationTemplate/src/Gewinner.java
+++ 2_Quellcode/ApplicationTemplate/src/Gewinner.java
@@ -1,5 +1,3 @@
-package pack1;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -11,49 +9,49 @@ public class Gewinner{
 		
 		JPanel panel = new JPanel(); //neuen Bereich erstellen
 		JFrame frame = new JFrame(); // neues Fenster erstellen
-		frame.setSize(600, 600); // Größe des Fensters festlegen
+		frame.setSize(600, 600); // GrÃ¶ÃŸe des Fensters festlegen
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // damit das Fenster geschlossen werden kann, wenn man auf das rote x klickt
 		frame.setLocationRelativeTo(null); //Fenster zentrieren
-		frame.setResizable(false); //damit man es nicht größer oder kleiner machen kann
+		frame.setResizable(false); //damit man es nicht grÃ¶ÃŸer oder kleiner machen kann
 		frame.setTitle("Gewinner"); //Fenster einen Titel geben
-		frame.add(panel); //Bereich dem Fenster hinzufügen
+		frame.add(panel); //Bereich dem Fenster hinzufÃ¼gen
 		
 		panel.setLayout(null);
 		
 		JLabel label = new JLabel("Du hast gewonnen!!"); //neues Label erstellen
-		label.setBounds(300,120, 280, 45); // Größe festlegen
-		label.setFont(label.getFont().deriveFont(25f)); //Schriftgröße festlegen
+		label.setBounds(300,120, 280, 45); // GrÃ¶ÃŸe festlegen
+		label.setFont(label.getFont().deriveFont(25f)); //SchriftgrÃ¶ÃŸe festlegen
 		label.setVisible(true);
-		frame.add(label); //Label dem Fenster hinzufügen 
+		frame.add(label); //Label dem Fenster hinzufÃ¼gen 
 		
 		JLabel label2 = new JLabel("vs"); //neues Label erstellen
-		label2.setBounds(250,50, 50, 45); // Größe festlegen
-		label2.setFont(label2.getFont().deriveFont(20f)); //Schriftgröße festlegen
+		label2.setBounds(250,50, 50, 45); // GrÃ¶ÃŸe festlegen
+		label2.setFont(label2.getFont().deriveFont(20f)); //SchriftgrÃ¶ÃŸe festlegen
 		label2.setVisible(true);
-		frame.add(label2); //Label dem Fenster hinzufügen 
+		frame.add(label2); //Label dem Fenster hinzufÃ¼gen 
 		
 		JLabel label3 = new JLabel("Spieler 1"); //neues Label erstellen
-		label3.setBounds(100,50, 150, 45); // Größe festlegen
-		label3.setFont(label3.getFont().deriveFont(20f)); //Schriftgröße festlegen
+		label3.setBounds(100,50, 150, 45); // GrÃ¶ÃŸe festlegen
+		label3.setFont(label3.getFont().deriveFont(20f)); //SchriftgrÃ¶ÃŸe festlegen
 		label3.setVisible(true);
-		frame.add(label3); //Label dem Fenster hinzufügen 
+		frame.add(label3); //Label dem Fenster hinzufÃ¼gen 
 		
 		JLabel label4 = new JLabel("Spieler 2"); //neues Label erstellen
-		label4.setBounds(350,50, 150, 45); // Größe festlegen
-		label4.setFont(label4.getFont().deriveFont(20f)); //Schriftgröße festlegen
+		label4.setBounds(350,50, 150, 45); // GrÃ¶ÃŸe festlegen
+		label4.setFont(label4.getFont().deriveFont(20f)); //SchriftgrÃ¶ÃŸe festlegen
 		label4.setVisible(true);
-		frame.add(label4); //Label dem Fenster hinzufügen 
+		frame.add(label4); //Label dem Fenster hinzufÃ¼gen 
 		
 		JLabel label5 = new JLabel("Spieler"); //neues Label erstellen
-		label5.setBounds(100,120, 150, 45); // Größe festlegen
-		label5.setFont(label5.getFont().deriveFont(20f)); //Schriftgröße festlegen
+		label5.setBounds(100,120, 150, 45); // GrÃ¶ÃŸe festlegen
+		label5.setFont(label5.getFont().deriveFont(20f)); //SchriftgrÃ¶ÃŸe festlegen
 		label5.setVisible(true);
-		frame.add(label5); //Label dem Fenster hinzufügen 
+		frame.add(label5); //Label dem Fenster hinzufÃ¼gen 
 		
 		JButton button = new JButton ("Gewinnstatistik"); //neuen Button erstellen
-		button.setBounds(225, 450, 150, 45); // Größe festlegen
+		button.setBounds(225, 450, 150, 45); // GrÃ¶ÃŸe festlegen
 		button.setVisible(true);
-		frame.add(button); //Button dem Panel hinzufügen
+		frame.add(button); //Button dem Panel hinzufÃ¼gen
 	
 		Draw draw = new Draw();
 		draw.setBounds(0, 0, 600, 600);
