1b25a68e4116c0384823ea44ccd8e94f1e73d703
GlunzVerena <102169565+GlunzVerena@users.noreply.github.com>
2022-04-27 20:50:04 +0200

    minor changes

--- 2_Quellcode/ApplicationTemplate/src/GameScreen.java
+++ 2_Quellcode/ApplicationTemplate/src/GameScreen.java
@@ -3,14 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-/**
- * The Game Screen is the Screen the actual game is played on. It contains the game field,
- * as well as an info box giving further explanations to the players.
- * The player names entered in WelcomeScreen are being transfered to and displayed on this screen.
- *  
- * @author Weiser
- *
- */
+
 public class GameScreen extends JFrame implements ActionListener{
    
 private JPanel panel;
@@ -20,21 +13,12 @@ private JLabel Pl2;
 static JButton newGame;
 private JFrame frame;
 private JButton shortcut;
-<<<<<<< Updated upstream
-
-/**
- * Constructs and initializes the game screen with set parameters.
- */
-	public GameScreen () {
-		//Initialisierungen
-=======
 Spieler spieler1test;
 Spieler spieler2test;
 	public GameScreen(Spieler spieler1, Spieler spieler2) {
 		spieler1test = spieler1;
 		spieler2test = spieler2;
 		//board = new Spielbrett();
->>>>>>> Stashed changes
 		frame = new JFrame();
 		panel = new JPanel();
 		info = new JLabel();
@@ -43,16 +27,18 @@ Spieler spieler2test;
 		newGame = new JButton("EndScreen");
 		shortcut = new JButton();
 		
-		//frame settings
 		frame.add(panel, BorderLayout.CENTER);
+		//frame.add(board);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//frame.pack();
 		frame.setSize(1024, 680);
 		frame.setLocationRelativeTo(null);
 		frame.setTitle("Mühle");
-		
-		//Panel settings
+			
+		//panel.setBackground(new Color(60, 179, 113));
 		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
 		panel.setLayout(null);
+		//panel.setVisible(true);
 		panel.setOpaque(true);;
 		panel.add(info);
 		panel.add(Pl1);
@@ -60,7 +46,9 @@ Spieler spieler2test;
 		panel.add(newGame);
 		panel.add(shortcut);
 		
-		//Button settings
+		//board.setBounds(275, 130, 450, 450);
+		//board.setVisible(true);
+		
 		newGame.setBounds(800, 25, 150, 25);
 		newGame.setText("Neues Spiel");
 		newGame.setVisible(true);
@@ -69,7 +57,6 @@ Spieler spieler2test;
 		shortcut.setText("EndScreen");
 		shortcut.addActionListener(this);
 		
-		//Label settings
 		info.setBounds(200, 85, 600, 25);
 		info.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		info.setBackground(Color.WHITE);
